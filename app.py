50e5d03c8da1a7fd6593e7d5ea9054a5fe66fdb2
Shivambind1 <skbindjnp@gmail.com>
2025-07-08 12:07:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,5 +192,4 @@ if st.button("✅ Generate Edited Images"):
                 img_bytes.seek(0)
                 
                 timestamp = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S-%f")
-                file_name = f"{name}_{timestamp}_variant_{i+1}.jpg"  # Closing the f-string correctly
-                download_links.append(st.download_button(label=f"Download {file
+                file_name
